5e7c1edbd14274bbf1d32e797b63deb4bcc870e8
Kuravi H <kuravih@gmail.com>
2024-03-15 21:41:54 -0400

    Update logging setup

--- src/pykato/log.py
+++ src/pykato/log.py
@@ -1,5 +1,8 @@
 import logging
 
-stream_handler = logging.StreamHandler()
-stream_handler.terminator = "\r"
-logging.basicConfig(format="[%(asctime)15s] : %(levelname)-8s : %(message)s", level=logging.INFO, datefmt="%Y%m%d.%H%M%S", force=True, handlers=[stream_handler])
+
+def setup_logger(name=None, terminator=""):
+    stream_handler = logging.StreamHandler()
+    stream_handler.terminator = terminator
+    logging.basicConfig(format="[%(asctime)15s] : %(levelname)-8s : %(message)s", level=logging.INFO, datefmt="%Y%m%d.%H%M%S", force=True, handlers=[stream_handler])
+    return logging.getLogger(name)
