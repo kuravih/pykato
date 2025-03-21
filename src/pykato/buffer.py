e1c7af08205761d7f237c3826ee63dd3fe91cadd
Kuravi H <kuravih@gmail.com>
2025-03-21 14:42:48 -0400

    update pop_back to pop multiple elements

--- src/pykato/buffer.py
+++ src/pykato/buffer.py
@@ -28,38 +28,41 @@ class NumpyCircularBuffer:
             Retrieve an element from the front.
 
     Usage:
-        buffer = NumpyQueue(5, dtype = np.dtype([("rep", "i4"), ("data", np.complex64, (200,200))]))
+        buffer = NumpyCircularBuffer(5, dtype = np.dtype([("rep", "i4"), ("data", np.complex64, (200,200))]))
 
     """
 
     def __init__(self, max_size: int = 1, dtype=np.float32):
         self.dtype = dtype
-        self.max_size = max_size
-        self._queue = np.zeros(self.max_size, dtype=self.dtype)
-        self.front = 0
-        self.rear = -1
-        self.size = 0
+        self._max_size = max_size
+        self._queue = np.empty(self._max_size, dtype=self.dtype)
+        self._front = 0
+        self._back = -1
+        self._size = 0
+
+    @property
+    def max_size(self) -> int:
+        return self._max_size
 
     def is_empty(self) -> bool:
         """
         Check if the queue empty.
         """
-        return self.size == 0
+        return self._size == 0
 
     def is_full(self) -> bool:
         """
         Check if the queue is full.
         """
-        return self.size == self.max_size
+        return self._size == self._max_size
 
     def empty(self):
         """
         Empty the queue
         """
-        self.front = 0
-        self.rear = -1
-        self.size = 0
-        self._queue = np.empty(self.max_size, dtype=self.dtype)
+        self._front = 0
+        self._back = -1
+        self._size = 0
 
     def push_back(self, element):
         """
@@ -67,27 +70,33 @@ class NumpyCircularBuffer:
         """
         if self.is_full():
             raise OverflowError("Queue is full")
-        self.rear = (self.rear + 1) % self.max_size
-        self._queue[self.rear] = element
-        self.size += 1
+        self._back = (self._back + 1) % self._max_size
+        self._queue[self._back] = element
+        self._size = self._size + 1
 
-    def pop_front(self):
+    def pop_front(self, count: int = 1):
         """
         Retrieve an element from the front.
         """
         if self.is_empty():
             raise IndexError("Queue is empty")
-        element = self._queue[self.front]
-        self.front = (self.front + 1) % self.max_size
-        self.size -= 1
-        return element
+        elements = self._queue[self._front : self._front + count]
+        self._front = (self._front + count) % self._max_size
+        self._size -= count
+        return elements
+
+    @property
+    def back(self) -> int:
+        return self._back
 
     @property
     def latest(self):
         """
-        latest element
+        Latest element in the buffer.
         """
-        return self._queue[self.rear]
+        if self.is_empty():
+            raise IndexError("Buffer is empty")
+        return self._queue[self._back]
 
     @property
     def queue(self) -> np.ndarray:
@@ -95,8 +104,7 @@ class NumpyCircularBuffer:
         element queue
         """
         if self.is_empty():
-            return np.empty(self.max_size, dtype=self.dtype)
-        if self.rear >= self.front:
-            return self._queue[self.front : self.rear + 1]
-        else:
-            return np.concatenate((self._queue[self.front :], self._queue[: self.rear + 1]), axis=0)
+            return np.array([], dtype=self.dtype)  # Return an empty array instead of a preallocated empty one
+        if self._back >= self._front:
+            return self._queue[self._front : self._back + 1]
+        return np.concatenate((self._queue[self._front :], self._queue[: self._back + 1]), axis=0)
--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -8,3 +8,28 @@ class TestNumpyCircularBuffer(unittest.TestCase):
     def test_NumpyCircularBuffer(self):
         buffer = NumpyCircularBuffer(4, dtype=np.dtype([("probe", "i4"), ("data", np.complex64, (10,10))]))
         self.assertIsInstance(buffer, NumpyCircularBuffer, "Buffer could not be created")
+
+    def test_NumpyCircularBuffer_push_back(self):
+        buffer = NumpyCircularBuffer(10, dtype=float)
+        self.assertIsInstance(buffer, NumpyCircularBuffer, "Buffer could not be created")
+
+        test_data = np.random.rand(10)
+        for index, value in enumerate(test_data):
+            buffer.push_back(value)
+            self.assertEqual(buffer.queue.tolist(), test_data[:index+1].tolist(), "Queues not equal")
+
+    def test_NumpyCircularBuffer_pop_front(self):
+        buffer = NumpyCircularBuffer(10, dtype=float)
+        self.assertIsInstance(buffer, NumpyCircularBuffer, "Buffer could not be created")
+
+        test_data = np.random.rand(6)
+        for index, value in enumerate(test_data):
+            buffer.push_back(value)
+
+        elements = buffer.pop_front()
+        self.assertEqual(elements.tolist(), test_data[:1].tolist(), "Queues not equal")
+        self.assertEqual(buffer.queue.tolist(), test_data[1:].tolist(), "Queues not equal")
+
+        elements = buffer.pop_front(2)
+        self.assertEqual(buffer.queue.tolist(), test_data[3:].tolist(), "Queues not equal")
+
